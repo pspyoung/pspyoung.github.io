1c64edad1e0838578144defec413463dea450f62
pspyoung <pspyoung2@gmail.com>
2022-04-23 16:59:29 -0700

    Basic information and links for 'connect with me' all listed. Uploads resume and pic locally

--- src/Components/Connect/Connect.js
+++ src/Components/Connect/Connect.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+function Connect(email, label) {
+	return (
+		<Container>
+			<li>
+				<a href="https://www.linkedin.com/in/pspyoung/">LinkedIn</a>
+			</li>
+			<li>
+				<a href="https://github.com/pspyoung">GitHub</a>
+			</li>
 
-function Connect() {
-	return <div>Link Resume Here</div>;
+			<li>
+				<a href="/resume/PeterResume.pdf">Resume</a>
+			</li>
+			<li>
+				<a href="mailto:pspyoung2@gmail.com">Email Me</a>
+			</li>
+		</Container>
+	);
 }
 
 export default Connect;
